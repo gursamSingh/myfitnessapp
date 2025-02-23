6dece51548fb535e1dedf7b37269fa856a0059db
gursam <gursam.madan@gmail.com>
2025-02-24 02:20:21 +0530

    Created a schema and user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema({
+  fullname: {
+    firstname: {
+      type: String,
+      required: true,
+      minlength: [3, "First name must be atleast 3 characters long "],
+    },
+    lastname: {
+      type: String,
+      minlength: [3, "Last name must be atleast 3 characters long"],
+    },
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: [3, "Email must be atleast  3 character long"],
+  },
+
+  password: {
+    type: String,
+    required: true,
+    select: false, //select option set to false for the password field. This ensures that the password field is excluded from query results.
+  },
+});
+
+// Generates a JWT token for user authentication.
+// The token contains the user's unique ID (_id) and expires in 24 hours.
+// JWT is used to securely authenticate users without needing to store session data.
+// The secret key (JWT_SECRET) is stored in environment variables for security.
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "24h",
+  });
+  return token;
+};
+
+// Hashes a plain text password before storing it in the database.
+// Uses bcrypt.hash() with 10 salt rounds to increase security and prevent brute-force attacks.
+// The hashed password is stored instead of the plain text version, ensuring user data remains secure.
+userSchema.methods.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+
+// Compares the entered password with the hashed password stored in the database.
+// bcrypt.compare() hashes the entered password and checks if it matches the stored hash.
+// Returns 'true' if passwords match, otherwise returns 'false'.
+// This ensures that even if passwords are stored as hashes, they can still be validated securely.
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
